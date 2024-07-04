f68f46178ccf420adf359ec46f11e9efc8432278
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-07-04 10:27:12 -0600

    Initial Commit.

---
+++ MovieCatalog/Models/MoviesDatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieCatalog.Models;
+
+internal static class MoviesDatabase
+{
+    public static async Task<IEnumerable<Movie>> GetMovies()
+    {
+        await using var stream = await FileSystem.OpenAppPackageFileAsync("data.json");
+
+        System.Text.Json.JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+
+        IEnumerable<Movie>? movies = await System.Text.Json.JsonSerializer.DeserializeAsync<List<Movie>>(stream, options);
+
+        return movies ?? [];
+    }
+}
