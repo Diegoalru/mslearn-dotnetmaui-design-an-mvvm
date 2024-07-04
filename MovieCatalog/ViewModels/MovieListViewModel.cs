61266fc0cafa0bb5416b3aa764d44e85141e6af9
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-07-04 11:09:57 -0600

    Create command.

--- MovieCatalog/ViewModels/MovieListViewModel.cs
+++ MovieCatalog/ViewModels/MovieListViewModel.cs
@@ -1,15 +1,21 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MovieCatalog.Models;
 
 namespace MovieCatalog.ViewModels;
 
-public class MovieListViewModel: ObservableObject
+public class MovieListViewModel : ObservableObject
 {
     public ObservableCollection<MovieViewModel> Movies { get; set; }
+    public ICommand DeleteMovieCommand { get; set; }
     private MovieViewModel? _selectedMovie;
 
-    public MovieListViewModel() =>
+    public MovieListViewModel()
+    {
         Movies = [];
+        DeleteMovieCommand = new Command<MovieViewModel>(DeleteMovie);
+    }
 
     public MovieViewModel? SelectedMovie
     {
@@ -19,12 +25,14 @@ public class MovieListViewModel: ObservableObject
 
     public async Task RefreshMovies()
     {
-        IEnumerable<Models.Movie> moviesData = await Models.MoviesDatabase.GetMovies();
+        var moviesData = await MoviesDatabase.GetMovies();
 
-        foreach (Models.Movie movie in moviesData)
+        foreach (var movie in moviesData)
             Movies.Add(new MovieViewModel(movie));
     }
 
-    public void DeleteMovie(MovieViewModel movie) =>
+    private void DeleteMovie(MovieViewModel movie)
+    {
         Movies.Remove(movie);
+    }
 }
