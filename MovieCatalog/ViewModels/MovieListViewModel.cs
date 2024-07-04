727a8722f2396feae7570eceb37bac94396742c3
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-07-04 10:27:41 -0600

    Add selection support.

--- MovieCatalog/ViewModels/MovieListViewModel.cs
+++ MovieCatalog/ViewModels/MovieListViewModel.cs
@@ -6,9 +6,16 @@ namespace MovieCatalog.ViewModels;
 public class MovieListViewModel: ObservableObject
 {
     public ObservableCollection<MovieViewModel> Movies { get; set; }
+    private MovieViewModel? _selectedMovie;
 
     public MovieListViewModel() =>
         Movies = [];
+
+    public MovieViewModel? SelectedMovie
+    {
+        get => _selectedMovie;
+        set => SetProperty(ref _selectedMovie, value);
+    }
 
     public async Task RefreshMovies()
     {
